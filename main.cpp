a9f4eadc0bd5ab9a63b95c5bc31f3811e6a3d40c
Anthony Bliss <124477160+AnthonyB3907@users.noreply.github.com>
2024-04-28 21:31:08 -0700

    Version 1.0

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Anthony
+ *
+ * Created on April 24, 2024, 10:15 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "login.h"
+
+
+int main(int argc, char** argv) {
+    
+    Login();
+
+    return 0;
+}
+
